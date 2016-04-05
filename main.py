09c1f92400530c5bafb6360984ef86362a4e57f0
jb567 <jb567@users.noreply.github..com>
2016-04-05 17:07:57 +1200

    Started program structure

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 __author__ = 'jb567'
 import praw
-import json
-import gspread
-import re
-from oauth2client.client import SignedJwtAssertionCredentials
+from pprint import pprint
+#{{METHODS
+def lords(s):
+    print('lords: ' + str(s.url))
+
+def pleb(s):
+    print('commons: ' + str(s.url))
+#}}
 
 r = praw.Reddit('Model House of Commons: Division Notifier - version 1')
 u = str(input('Reddit Username: '))
@@ -13,3 +17,15 @@ sub = r.get_subreddit('mhocmp+mholvote')
 lastSub = ''
 #Start Loop here in production
 new = sub.get_new(limit=10)
+
+for submission in new:
+#TODO
+    if submission.id == lastSub:
+#If the loop has already submitted it
+        break
+    if 'mhocmp' in submission.url.lower():
+        pleb(submission)
+    elif 'mholvote' in submission.url.lower():
+        lords(submission)
+
+
